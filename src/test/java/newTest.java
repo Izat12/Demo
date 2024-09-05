738f8fdcc8675caf1da4f7e962ee9392f7f531f0
Dzhamilya <sherimbekovadjama@gmail.com>
2024-09-05 12:24:37 +0600

    Добавила новый тест класс для проверки 111

--- src/test/java/newTest.java
+++ src/test/java/newTest.java
@@ -2,5 +2,6 @@ public class newTest {
     public static void main(String[] args) {
         System.out.println("Проверка создания нового класса");
         System.out.println("новый");
+        System.out.println("Новые изменения");
     }
 }
