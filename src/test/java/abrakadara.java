9a88b0cf7f47d171923d6edbcabff74bcb92ecf4
Gaukhar <hayannahodeki@gmail.com>
2024-09-05 11:58:07 +0600

    это Авадакедавраааа!!!

--- src/test/java/abrakadara.java
+++ src/test/java/abrakadara.java
@@ -1,5 +1,8 @@
 public class abrakadara {
     public static void main(String[] args) {
         System.out.println("avadakedavra");
+        System.out.println(" чай ичелик! пс Janar Clod Van DAmych");
+        System.out.println("Айбек покинул встречу");
+        System.out.println("");
     }
 }
