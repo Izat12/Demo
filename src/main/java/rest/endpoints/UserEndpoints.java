a707de4ff993efd3572d801b337893eca2a10690
ZHYLDYZ ADYLCHAEVA <asylbash@gmail.com>
2024-09-06 01:13:37 +0200

    new endpoints added

--- src/main/java/rest/endpoints/UserEndpoints.java
+++ src/main/java/rest/endpoints/UserEndpoints.java
@@ -4,12 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserEndpoints {
-    public static final String URL ="https://24winter.talentlms.com";
+    public static final String URL ="https://dnwinter2024.talentlms.com";
 
     public static final String V1 ="v1";
     public static final String USERS ="users";
     public static final String USER_SIGNUP ="usersignup";
     public static final String DELETE_USER ="deleteuser";
+    public static final String EDIT_USER = "edituser";
+
+    public static final String COURSES = "courses";
+    public static final String CREATE_COURSE = "createcourse";
+    public static final String DELETE_COURSE = "deletecourse";
+    public static final String ADD_USER_TO_COURSE = "addusertocourse";
+    public static final String REMOVE_USER_FROM_COURSE = "removeuserfromcourse";
+    public static final String BUY_COURSE = "buycourse";
+    public static final String GET_TEST_ANSWERS = "gettestanswers";
+    public static final String GET_ILT_SESSIONS = "getiltsessions";
+
+    public static final String GROUPS = "groups";
+    public static final String CREATE_GROUP = "creategroup";
+    public static final String DELETE_GROUP = "deletegroup";
+    public static final String ADD_USER_TO_GROUP = "addusertogroup";
+    public static final String REMOVE_USER_FROM_GROUP = "removeuserfromgroup";
+    public static final String ADD_COURSE_TO_GROUP = "addcoursetogroup";
 
 
     public static Map<String, String> BASE_HEADERS = new HashMap<>() {{
