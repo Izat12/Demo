ca0b0732d9f328547dfa91281e7080016055053f
Dzhamilya <sherimbekovadjama@gmail.com>
2024-09-05 12:08:16 +0600

    Гаухар

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,9 +1,8 @@
 public class Test1 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hi");
-=======
+
         System.out.println("He");
->>>>>>> e586ec792a42e81a0d0f6b6bd77fe816415ac9d2
+
     }
 }
