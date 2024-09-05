23de3085c0f7ae6258112db6f3f32f07df9e2c8b
ZHYLDYZ ADYLCHAEVA <asylbash@gmail.com>
2024-09-06 01:12:00 +0200

    Added new method post

--- src/main/java/rest/controller/UserController.java
+++ src/main/java/rest/controller/UserController.java
@@ -1,9 +1,10 @@
 package rest.controller;
 
 import io.restassured.response.Response;
-import org.apiguardian.api.API;
 import rest.ApiMethods;
+import rest.pojo.rerponse.Users;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static rest.endpoints.UserEndpoints.*;
@@ -19,4 +20,14 @@ public class UserController extends ApiMethods {
         return this.response = super.get(getEndpoint(API, V1, USERS));
     }
 
+    public Users createUser(Users user) {
+        return super.post(getEndpoint(API, V1, USER_SIGNUP), user.toJson()).as(Users.class);
+    }
+    public void deleteUser(String userId) {
+        Map<String, String> params = new HashMap<>() {{
+            put("user_id", userId);
+            put("deleted_by_user_id", "1");
+        }};
+        super.post(getEndpoint(API, V1, DELETE_USER), params);
+    }
 }
